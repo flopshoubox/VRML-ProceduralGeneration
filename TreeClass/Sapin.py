a60da86b8337c35f501c1a4ffbd6398a1b4bdeeb
Alexis CARE <alx.care@gmail.com>
2016-10-20 17:43:40 -0400

    Debug + write into file + random

--- TreeClass/Sapin.py
+++ TreeClass/Sapin.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+import os
+from random import randint
+from random import uniform
 
 
 class Sapin:
@@ -13,120 +16,75 @@ class Sapin:
     spineTrunk = [0,0.5,0,0,1,0,0,1.5,0] #2
     crossSectionTrunk = [1,0,0.707106,-0.707106,0,-1,-0.707106,-0.707106,-1,0,-0.707106,0.707106,0,1,0.707106,0.707106,1,0] #2
 
-    posLeaf1 = [0, 1, 0]
-    scaleLeaf1G = [1,1,1]
-    spineLeaf1 = [0,0.5,0,0,1,0,0,1.5,0]
-    crossSectionLeaf1 = [-1,0,-0.707106,0.707106,0,1, 0.707106,0.707106,1,0,0.866025,0.5,0.866025,-0.5,0,-1, -0.866025,-0.5,-0.866025,0.5,0,1,0.866025,0.5]
-    scaleLeaf1 =  [1,1,1.4,1.4,0.8,0.8,0.4,0.4,0,0]
+    posLeaf = [0, 1, 0]
+    scaleLeafG = [1,1,1]
+    spineLeaf = [0,0.5,0,0,0.65,0,0,1.5,0,0,2,0,0,2.5,0]
+    crossSectionLeaf = [0.866025,0.5,0.866025,-0.5,0,-1, -0.866025,-0.5,-0.866025,0.5,0,1,0.866025,0.5]
+    scaleLeaf =  [1,1,1.4,1.4,0.8,0.8,0.4,0.4,0,0]
 
-    posLeaf2 = [0, 3, 0]
-    scaleLeaf2G = [0.8,0.8,0.8]
-    spineLeaf2 = [0,0.5,0,0,1,0,0,1.5,0]
-    crossSectionLeaf2 = [-1,0,-0.707106,0.707106,0,1, 0.707106,0.707106,1,0,0.866025,0.5,0.866025,-0.5,0,-1, -0.866025,-0.5,-0.866025,0.5,0,1,0.866025,0.5]
-    scaleLeaf2 =  [1,1,1.4,1.4,0.8,0.8,0.4,0.4,0,0]
-
-    posLeaf3 = [0, 4, 0]
-    scaleLeaf3G = [0.5,0.5,0.5]
-    spineLeaf3 = [0,0.5,0,0,1,0,0,1.5,0]
-    crossSectionLeaf3 = [-1,0,-0.707106,0.707106,0,1, 0.707106,0.707106,1,0,0.866025,0.5,0.866025,-0.5,0,-1, -0.866025,-0.5,-0.866025,0.5,0,1,0.866025,0.5]
-    scaleLeaf3 =  [1,1,1.4,1.4,0.8,0.8,0.4,0.4,0,0]
 
 
     def __init__(self, X=0, Y=0, Z=0):
         self.posTree = [X,Y,Z]
 
     def toString(self):
-        stri = "Transform {\n"
+        stri = "#VRML V2.0 utf8\n\n"
+
+        stri += "Transform {\n"
         stri += "\ttranslation " + "  ".join(str(x) for x in self.posTree) + "\n"
         stri += "\tscale " + "  ".join(str(x) for x in self.scaleTree) + "\n"
-        stri += "\tTransform {\n"
-        stri += "\t\tscale " + "  ".join(str(x) for x in self.posTree) + "\n"
-        stri += "\t\tChildren [\n"
-        stri += "\t\t\tShape {\n"
-        stri += "\t\t\t\tappearance Appearance {\n"
-        stri += "\t\t\t\t\tmaterial Material {\n"
-        stri += "\t\t\t\t\t\tdiffuseColor " + "  ".join(str(x) for x in self.colorTrunk) + "\n"
+        stri += "\tchildren [\n"
+
+
+        for i in range(0,3):
+            self.posLeaf = [0, 1 + i, 0]
+            self.scaleLeafG = [1 - i *0.2,1 - i *0.2,1 - i *0.2]
+
+            stri += "\t\tTransform {\n"
+            stri += "\t\t\ttranslation " + "  ".join(str(x) for x in self.posLeaf) + "\n"
+            stri += "\t\t\tscale " + "  ".join(str(x) for x in self.scaleLeafG) + "\n"
+            stri += "\t\t\tchildren [\n"
+            stri += "\t\t\t\tShape {\n"
+            stri += "\t\t\t\t\tappearance	Appearance {\n"
+            stri += "\t\t\t\t\t\tmaterial Material {\n"
+            stri += "\t\t\t\t\t\t\tdiffuseColor "+ " ".join(str(x) for x in self.colorLeaf) + "\n"
+            stri += "\t\t\t\t\t\t}\n"
+            stri += "\t\t\t\t\t}\n"
+            stri += "\t\t\t\t\tgeometry Extrusion {\n"
+            stri += "\t\t\t\t\t\tccw TRUE\n"
+            stri += "\t\t\t\t\t\tspine [\n"
+            stri += listStr3(self.spineLeaf, 6) + "]\n"
+            stri += "\t\t\t\t\tscale [\n"
+            stri += listStr2(self.scaleLeaf, 6) + "]\n"
+            stri += "\t\t\t\t\t\tcrossSection [\n"
+            stri += listStr2(self.crossSectionLeaf, 6) + "]\n"
+            stri += "\t\t\t\t\t}\n"
+            stri += "\t\t\t\t}\n"
+            stri += "\t\t\t]\n"
+            stri += "\t\t}\n"
+
+        stri += "\t\tTransform {\n"
+        stri += "\t\tscale " + "  ".join(str(x) for x in self.scaleTrunkG) + "\n"
+        stri += "\t\t\tchildren [\n"
+        stri += "\t\t\t\tShape {\n"
+        stri += "\t\t\t\t\tappearance Appearance {\n"
+        stri += "\t\t\t\t\t\tmaterial Material {\n"
+        stri += "\t\t\t\t\t\t\tdiffuseColor " + "  ".join(str(x) for x in self.colorTrunk) + "\n"
+        stri += "\t\t\t\t\t\t}\n"
+        stri += "\t\t\t\t\t}\n"
+        stri += "\t\t\t\t\tgeometry Extrusion {\n"
+        stri += "\t\t\t\t\t\tccw TRUE\n"
+        stri += "\t\t\t\t\t\tspine [\n"
+        stri += listStr3(self.spineTrunk, 6) + "]\n"
+        stri += "\t\t\t\t\t\tscale [\n"
+        stri += listStr2(self.scaleTrunk, 6) + "]\n"
+        stri += "\t\t\t\t\t\tcrossSection [\n"
+        stri += listStr2(self.crossSectionTrunk, 6) + "]\n"
         stri += "\t\t\t\t\t}\n"
         stri += "\t\t\t\t}\n"
-        stri += "\t\t\t\tgeometry Extrusion {\n"
-        stri += "\t\t\t\t\tccw TRUE\n"
-        stri += "\t\t\t\t\tspine [\n"
-        stri += listStr3(self.spineTrunk, 5) + "]\n"
-        stri += "\t\t\t\t\tscale [\n"
-        stri += listStr2(self.scaleTrunk, 5) + "]\n"
-        stri += "\t\t\t\t\tcrossSection [\n"
-        stri += listStr2(self.crossSectionTrunk, 5) + "]\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t}\n"
-        stri += "\t\t]\n"
-        stri += "\t}\n"
-
-        stri += "\tTransform {\n"
-        stri += "\t\ttranslation " + "  ".join(str(x) for x in self.posLeaf1) + "\n"
-        stri += "\t\tscale " + "  ".join(str(x) for x in self.scaleLeaf1G) + "\n"
-        stri += "\t\tChildren [\n"
-        stri += "\t\t\tShape {\n"
-        stri += "\t\t\t\tappearance	Appearance {\n"
-        stri += "\t\t\t\t\tmaterial Material {\n"
-        stri += "\t\t\t\t\t\tdiffuseColor "+ " ".join(str(x) for x in self.colorLeaf) + "\n"
-        stri += "\t\t\t\t\t}\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t\tgeometry Extrusion {\n"
-        stri += "\t\t\t\t\tspine [\n"
-        stri += listStr3(self.spineLeaf1, 5) + "]\n"
-        stri += "\t\t\t\t\tscale [\n"
-        stri += listStr3(self.scaleLeaf1, 5) + "]\n"
-        stri += "\t\t\t\t\tcrossSection [\n"
-        stri += listStr3(self.crossSectionLeaf1, 5) + "]\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t}\n"
-        stri += "\t\t]\n"
-        stri += "\t}\n"
-
-        stri += "\tTransform {\n"
-        stri += "\t\ttranslation " + "  ".join(str(x) for x in self.posLeaf2) + "\n"
-        stri += "\t\tscale " + "  ".join(str(x) for x in self.scaleLeaf2G) + "\n"
-        stri += "\t\tChildren [\n"
-        stri += "\t\t\tShape {\n"
-        stri += "\t\t\t\tappearance	Appearance {\n"
-        stri += "\t\t\t\t\tmaterial Material {\n"
-        stri += "\t\t\t\t\t\tdiffuseColor "+ " ".join(str(x) for x in self.colorLeaf) + "\n"
-        stri += "\t\t\t\t\t}\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t\tgeometry Extrusion {\n"
-        stri += "\t\t\t\t\tspine [\n"
-        stri += listStr3(self.spineLeaf2, 5) + "]\n"
-        stri += "\t\t\t\t\tscale [\n"
-        stri += listStr3(self.scaleLeaf2, 5) + "]\n"
-        stri += "\t\t\t\t\tcrossSection [\n"
-        stri += listStr3(self.crossSectionLeaf2, 5) + "]\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t}\n"
-        stri += "\t\t]\n"
-        stri += "\t}\n"
-
-        stri += "\tTransform {\n"
-        stri += "\t\ttranslation " + "  ".join(str(x) for x in self.posLeaf3) + "\n"
-        stri += "\t\tscale " + "  ".join(str(x) for x in self.scaleLeaf3G) + "\n"
-        stri += "\t\tChildren [\n"
-        stri += "\t\t\tShape {\n"
-        stri += "\t\t\t\tappearance	Appearance {\n"
-        stri += "\t\t\t\t\tmaterial Material {\n"
-        stri += "\t\t\t\t\t\tdiffuseColor "+ " ".join(str(x) for x in self.colorLeaf) + "\n"
-        stri += "\t\t\t\t\t}\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t\tgeometry Extrusion {\n"
-        stri += "\t\t\t\t\tspine [\n"
-        stri += listStr3(self.spineLeaf3, 5) + "]\n"
-        stri += "\t\t\t\t\tscale [\n"
-        stri += listStr3(self.scaleLeaf3, 5) + "]\n"
-        stri += "\t\t\t\t\tcrossSection [\n"
-        stri += listStr3(self.crossSectionLeaf3, 5) + "]\n"
-        stri += "\t\t\t\t}\n"
-        stri += "\t\t\t}\n"
-        stri += "\t\t]\n"
-        stri += "\t}\n"
-
+        stri += "\t\t\t]\n"
+        stri += "\t\t}\n"
+        stri += "\t]\n"
         stri += "}"
 
         return stri
@@ -155,4 +113,7 @@ def listStr2(list, tab):
 
 if __name__ == '__main__':
     myTree = Sapin()
-    print myTree.toString()
+    # print myTree.toString()
+    mon_fic = open('Sapin.wrl','w')
+    mon_fic.write(myTree.toString())
+    mon_fic.close()
